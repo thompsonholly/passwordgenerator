94a4db56ae18d02eacbd4ae321747349a27aba94
Holly T <144973465+thompsonholly@users.noreply.github.com>
2023-10-01 19:20:50 -0500

    create generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,107 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Global variables
+var userChoose;
+var passwordLength;
+var upperCase;
+var lowerCase;
+var includeNumbers;
+var includeSpecialCharacters;
 
-var userChoose
-var passwordLength
-var upperCase
-var lowerCase
-var number
+var finalPassword = "";
 
-// Password
-var password = {
-  passwordLength: 0,
-  min: 8,
-  max: 128,
+function getARandomNumber() {
+  return 9;
+}
+
+function passwordLength() {
+  return min = 8, max = 20;
+}
+
+function generatePassword() {
+  passwordLength = prompt("How many characters in length is your password? (Minimum of 8 characters and maximum amount of characters is 20")
+  includeNumbers = confirm("Do you want numbers?")
+  includeSpecialCharacters = confirm("Do you want to include special characters?")
+  getARandomNumber()
+  upperCase = 
+  var lowerCase
+
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Global variables
+
+
+
+
+
+
+
+
+
+
+
+
+var password = "";
+
+
+function addLowerCaseLetters() {
+  password = password + "abc"
+}
+
+function addUpperCaseLetters() {
+
+}
+
+
+function generatePassword() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Password
+var password = {
+  passwordLength = 8 >= 128
+  min: 8,
+  max: 128,
+}
+// Prompting user for number of characters in password
 function promptUserChoose() {
   userChoose = prompt("Please select number of characters- min length of 8, maximun length of 128")
-  console.log(`Choose: ${userChoose}`);
+  console.log(`User Choose: ${userChoose}`);
 }
 
 // Write password to the #password input
